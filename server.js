02192eddaf99483e6dfe0261e881bab8b98c38f6
Tereza Buriankova <tburiankova@gmail.com>
2020-08-20 17:21:39 +0100

    preparing for heroku deployment

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
@@ -44,9 +45,16 @@ app.use('/api/posts', posts);
 app.use('/api/users', users);
 
 // handling unexisting routes
-app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route', 404);
-  throw error;
+// app.use((req, res, next) => {
+//   const error = new HttpError('Could not find this route', 404);
+//   throw error;
+// });
+
+// serving the build folder
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 // error handler
